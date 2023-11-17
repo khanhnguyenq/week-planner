57805b88c05dd3dc6f01d38e72ac1ba7aac425b0
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-11-17 22:21:09 +0000

    Added Event Listeners for submit event.

--- main.js
+++ main.js
@@ -10,3 +10,19 @@ const $cancelBtn = document.querySelector('.cancel');
 $cancelBtn.addEventListener('click', function (event) {
   $overlay.classList.add('hidden');
 });
+
+let data = {
+  view: 'day',
+  entries: [],
+  editing: null,
+  nextEntryId: 1,
+};
+
+const $confirmBtn = document.querySelector('.confirm');
+const $eventForm = document.querySelector('.event-form');
+
+$confirmBtn.addEventListener('submit', function (event) {
+  const submittedData = {};
+});
+
+console.log('event-form:', $eventForm);
